a84e6402cfe7bfdd1af8e29611c8944036b9e748
NourAhmedM <48297357+NourAhmedM@users.noreply.github.com>
2020-06-01 18:52:12 +0200

    edit execution for flags

--- execution/ALU.vhd
+++ execution/ALU.vhd
@@ -26,14 +26,13 @@ architecture ALU_arch of ALU is
 	signal out_temp :  std_logic_vector(31 downto 0);
 	--signal temp :unsigned(31 downto 0);
 
+	signal result:std_logic_vector(32 downto 0);
+
 begin 
 
+
 	out_temp <=   not in_1  when (opcode="00001") else   --not
-		  std_logic_vector(signed(in_1)+signed(one)) when (opcode="00010") else   --inc
-		std_logic_vector(signed(in_1)-signed(one)) when (opcode="00011") else   --dec
-		std_logic_vector(signed(in_1)+signed(in_2)) when (opcode="01001") else   --add
-		std_logic_vector(signed(in_1)+signed(in_2)) when (opcode="01010") else   --Iadd
-		std_logic_vector(signed(in_1)-signed(in_2)) when (opcode="01011") else   --sub
+		   result(31 downto 0) when (opcode="00010" or opcode="00011" or opcode="01001" or opcode="01010" or opcode="01011") else   --inc dec add idd sub
 		in_1 and in_2	when (opcode="01100") else   --and
 		in_1 or in_2	when (opcode="01101") else   --or
 		std_logic_vector(shift_left(unsigned(in_1),to_integer(unsigned(in_2)))) when (opcode="01110") else   --shift left 
@@ -42,10 +41,13 @@ begin
 		std_logic_vector(signed(in_2)+signed(in_1)) when (opcode="10001" or opcode="11011" or opcode="11100") else    --pop --ret --rti 
          		"00000000000000000000000000000000" ;
 	
-
 	
-
-
+	result<=  std_logic_vector(signed('0' &in_1) +signed('0' &one)) when (opcode="00010") else --inc
+         	  std_logic_vector(resize(signed(in_1),33) -resize(signed(one),33) )when (opcode="00011") else --dec
+		std_logic_vector(signed('0' &in_1) +signed('0' &in_2)) when (opcode="01001") else --add
+		std_logic_vector(signed('0' &in_1) +signed('0' &in_2)) when (opcode="01010") else --Iadd
+		std_logic_vector(resize(signed(in_1),33) -resize(signed(in_2),33)) when (opcode="01011") else   --sub
+		"000000000000000000000000000000000" ;
 
 	output<=out_temp;
 
@@ -62,6 +64,7 @@ begin
 	----carry flag 
 	flags(2)<=   in_1(32-to_integer(unsigned(in_2)))when (opcode="01110" and to_integer(unsigned(in_2))<33 and  to_integer(unsigned(in_2)) /=0) else
 			in_1(to_integer(unsigned(in_2))-1) when (opcode="01111" and to_integer(unsigned(in_2))>0 and to_integer(unsigned(in_2))<33) else
+			result(32) when (opcode="00010" or opcode="00011" or opcode="01001" or opcode="01010" or opcode="01011")else
 			 '0';
 
 end ALU_arch;
